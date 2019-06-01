79afe91a6daf805333361b538281b26f1e219594
Giancarlo <giancarlo.segier@student.howest.be>
2019-06-01 19:45:38 +0200

    correct files structure added.

---
+++ client/src/containers/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import style from "./App.module.css";
+
+function App() {
+  return (
+    <div className={style.center}>
+      <h1>Meet the characters</h1>
+    </div>
+  );
+}
+
+export default App;
