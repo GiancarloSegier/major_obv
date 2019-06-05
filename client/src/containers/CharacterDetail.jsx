97e7682cab6b96dc940f545b93ce8a737066c802
Laurens Segaert <laurenssegaert@Laurenss-MacBook-Pro.local>
2019-06-05 16:46:33 +0200

    detail page main characters

--- client/src/containers/CharacterDetail.jsx
+++ client/src/containers/CharacterDetail.jsx
@@ -3,11 +3,11 @@ import { inject, observer } from "mobx-react";
 import styles from "./CharacterDetail.module.css";
 
 const CharacterDetail = ({ characterId, store }) => {
-  // const { characters } = store;
+  const { characters } = store;
 
-  // const current = characters.find(character => character.id === characterId);
-  // console.log(characters);
-  // console.log(current.name, current.nickname, current.about);
+  const current = characters.find(character => character.id === characterId);
+  console.log(characters);
+  console.log(current.name, current.nickname, current.about);
 
   return (
     <div className="container">
@@ -19,15 +19,9 @@ const CharacterDetail = ({ characterId, store }) => {
         />
 
         <div className={styles.characterDetail__info}>
-          <h2 className={styles.characterDetail__name}>Almaviva</h2>
-          <p className={styles.characterDetail__nickname}>
-            Onvermoeide playboy
-          </p>
-          <p className={styles.characterDetail__about}>
-            Everyone has their perfect match. Sometimes it’s just around the
-            corner, other times you have to travel the world in search of it.
-            Wherever your perfect cheeseburger is, it’s out there.
-          </p>
+          <h2 className={styles.characterDetail__name}>{current.name}</h2>
+          <p className={styles.characterDetail__nickname}>{current.nickname}</p>
+          <p className={styles.characterDetail__about}>{current.about}</p>
           <button>Beleef mijn verhaal</button>
         </div>
       </section>
