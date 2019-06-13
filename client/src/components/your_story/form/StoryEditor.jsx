98411dfff234be60b58c2e93232bd3ac560bc951
Giancarlo <giancarlo.segier@student.howest.be>
2019-06-13 18:48:26 +0200

    story editor finished

--- client/src/components/your_story/form/StoryEditor.jsx
+++ client/src/components/your_story/form/StoryEditor.jsx
@@ -5,7 +5,7 @@ import Hints from "./Hints";
 class StoryEditor extends Component {
   constructor(props) {
     super(props);
-    this.state = { textareaLength: 60 };
+    this.state = { textareaLength: 60, openHints: false };
     this.textRef = React.createRef();
     this.hints = this.props.store.hints;
   }
@@ -38,6 +38,7 @@ class StoryEditor extends Component {
     this.textRef.current.style.height = this.state.textareaLength + "px";
     console.log(e.currentTarget.value);
   };
+
   render() {
     return (
       <>
@@ -45,10 +46,21 @@ class StoryEditor extends Component {
           onSubmit={this.submitStory}
           className={`${styles.editorGrid} fadeIn`}
         >
+          <div className={styles.SM__editor_buttons}>
+            <button
+              type="button"
+              className={styles.hintButton}
+              onClick={() =>
+                this.setState({ openHints: !this.state.openHints })
+              }
+            />
+            <button type="submit" className={styles.saveButton} />
+          </div>
           <div className={styles.textEditor}>
             <h2 className={styles.part__title}>
               <span className={styles.part__step}>05</span> Jouw verhaal
             </h2>
+
             <input
               className={styles.titleField}
               name="title"
@@ -69,7 +81,19 @@ class StoryEditor extends Component {
             />
           </div>
 
-          <div className={styles.hints}>
+          <div
+            className={
+              this.state.openHints
+                ? `${styles.hints} ${styles.openHints}`
+                : `${styles.hints}`
+            }
+          >
+            <button
+              onClick={() =>
+                this.setState({ openHints: !this.state.openHints })
+              }
+              className={styles.close}
+            />
             <Hints />
           </div>
         </form>
